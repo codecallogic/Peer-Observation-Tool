afa740d2336d85811813faadef8fb601f3022ba9
Fabricio <contact@fabricioguardia.com>
2022-09-20 02:39:45 -0700

    react app to subdirectory

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Observations from './components/observations';
 import './styles/app.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router basename={'/poform'}>
     <div className="container">
       <h1 className="container-breadcrumb" onClick={() => window.location.href = 'https://ecostem.calstatela.edu/'}>Home</h1>
       <Observations></Observations>
     </div>
-    </>
+    </Router>
   );
 }
 
